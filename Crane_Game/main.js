716d6e458dacbd87cdbb9f6efe33d61ba401e552
sosongha3 <sosongha3@ajou.ac.kr>
2023-06-06 21:04:34 +0900

    branch coflict solve

--- Crane_Game/main.js
+++ Crane_Game/main.js
@@ -7,7 +7,16 @@ var gl; //gl은 WEBGL context object이다
 //matrix 
 var modelViewMatrixLoc;
 var projectionMatrixLoc;
+<<<<<<< HEAD
 var modelViewMatrix = scalem(0.01,0.01,0.01); //mat4()
+=======
+var modelViewMatrix = mat4();
+//var modelViewMatrix = scalem(0.01,0.01,0.01); //mat4()
+// var eye = vec3(0.0,0.0,1.0);
+// var at = vec3(0.0,0.0,0.0);
+// var up = vec3(0.0,0.1,0.0);
+// modelViewMatrix = mult(modelViewMatrix, lookAt(eye, at, up)); //viewing
+>>>>>>> ed64c11 (heirarchical & stack added)
 var projectionViewMatrix = perspective(100, 1, 0, 1);
 
 //shading
@@ -26,6 +35,51 @@ var diffuseProduct = mult(lightDiffuse, materialDiffuse);
 var specularProduct = mult(lightSpecular, materialSpecular);
 var drag = false;
 /**-------------------------- */
+ 
+var stack =[];
+var leftCraneAngle = [[0,0,0],[0,0,0],[0,0,0]];
+var rightCraneAngle = [[0,0,0],[0,0,0],[0,0,0]];
+
+var upperCraneStream = {x: 0.05, y: 0.4, z:  0.02};
+var CraneStream = {x: 2, y: 0.25, z: 1.2};
+var lowerCraneStream = {x: 0.1, y: 0.2, z:  0.1};
+var craneTorso = {x: 0.15, y: 0.3, z:  0.1};
+var lowerCraneTorso = {x: 0.4, y: 0.2, z:  0.4};
+var upperCrane = {x: 3.5, y: 1.7, z:  0.8};
+var mediumCrane = {x: 3.5, y: 1, z:  0.8};
+var lowerCrane = {x: 3.5, y: 1.0, z:  1.0};
+
+var craneAngle=[20,60,20];
+
+var humanTorso = {w: 25, h:25, d:10}; //width, height, depth를 뜻함
+var humanHead = {w: 20, h:20, d:20};
+var humanUpperArm = {w: 5, h:15, d:10};
+var humanLowerArm = {w: 5, h:15, d:10};
+var humanPelvis = {w: 25, h:15, d:10};
+var humanThigh = {w: 10, h:15, d:10};
+var humanCalf = {w: 10, h:15, d:10};
+var humanFoot = {w: 10, h:5, d:15};
+
+var humanAngle = []
+
+var drag = false;
+var redraw = false;
+var x,y;
+var lastX = 512;
+var lastY = 512;
+var dx = 0;
+var dy = 0;
+var theta = 0;
+var phi = 0;
+var time = 0;
+
+var figure = [];
+var numNodes = 11 + 13;
+for(var i = 0; i < numNodes; i++){
+    figure[i] = createNode(null, null, null, null);
+}
+
+var normalLoc;
 
 window.onload = function init() 
 {
@@ -36,8 +90,14 @@ window.onload = function init()
 
     
     /*------verctices 생성하기------*/
-    var vertices = humanTorso.concat(humanHead);
-    var normals = normal_humanTorso.concat(normal_humanHead);
+   
+    //var vertices = crane_vertices.concat(human_vertices);
+    //var normals = crane_normals.concat(human_normals);
+
+    var vertices = human_vertices;
+    var normals = human_normals;
+
+    console.log(vertices)
     /*------------------------------*/
 
     //  Configure WebGL
@@ -98,7 +158,9 @@ window.onload = function init()
      * 
      * 
      */
-    console.log(vertices);
+
+     console.log(vertices);
+
     render();
 };
 
@@ -118,15 +180,76 @@ function render() {
     cur_vertex = drawHumanTorso(cur_vertex);//함수 들어갔다 오면, cur_vetex는 humanTorso의 vertex 수만큼 더해져 나옴
     cur_vertex = drawHumanHead(cur_vertex);
     
+    // modelViewMatrix = mult(modelViewMatrix, translate(0.0, 1.0, 0.0));
+    // modelViewMatrix = mult(modelViewMatrix, rotateY(10));
+    // modelViewMatrix = mult(modelViewMatrix, rotateX(-10));
+    // modelViewMatrix = mult(modelViewMatrix, scalem(upperCraneStream.x,upperCraneStream.y,upperCraneStream.z));
+    // gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+   
+    // cur_vertex = drawUpperCraneStream(cur_vertex);
 
+    // modelViewMatrix = mult(modelViewMatrix, translate(0.0, -2*upperCraneStream.y, 0.0));
+    // modelViewMatrix = mult(modelViewMatrix, scalem(CraneStream.x,CraneStream.y,CraneStream.z));
     // gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
 
-    // gl.drawArrays( gl.TRIANGLES, 0, 3);
-    // // gl.drawArrays(gl.TRIANGLE_STRIP,0 ,3);
+    // cur_vertex = drawCraneStream(cur_vertex);
 
-    // setTimeout(
-    //     function(){requestAnimationFrame(render);}, 10000
-    // );
+    // modelViewMatrix = mult(modelViewMatrix, translate(0.0,-1.0, 0.0));
+    // gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+    // cur_vertex = drawLowerCraneStream(cur_vertex);
+
+    // modelViewMatrix = mult(modelViewMatrix, translate(0.0,-2, 0.0));
+    // modelViewMatrix = mult(modelViewMatrix, scalem(2.0, 3.0, 2.0));
+    // gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+    // cur_vertex = drawCraneTorso(cur_vertex);
+
+
+    // modelViewMatrix = mult(modelViewMatrix, translate(0.0, -0.5, 0.0));
+    // modelViewMatrix = mult(modelViewMatrix, scalem(lowerCraneTorso.x,lowerCraneTorso.y,lowerCraneTorso.z));
+    // gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+    // cur_vertex = drawLowerCraneTorso(cur_vertex);
+
+    // modelViewMatrix = mult(modelViewMatrix, translate(-3, -1.2, 0.0));
+    // stack.push(modelViewMatrix);
+    // stack.push(modelViewMatrix);
+    // modelViewMatrix = mult(modelViewMatrix, rotateZ(craneAngle[0]))
+    // modelViewMatrix = mult(modelViewMatrix, scalem(upperCrane.x,upperCrane.y,upperCrane.z));
+    // gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+
+    // var craneiniti = cur_vertex;
+    // cur_vertex = drawCrane(cur_vertex);
+
+    // cur_vertex= craneiniti;
+    // modelViewMatrix = stack.pop();
+    // modelViewMatrix = mult(modelViewMatrix, translate(6, 0.0, 0.0));
+    // modelViewMatrix = mult(modelViewMatrix, rotateY(180));
+    // stack.push(modelViewMatrix);
+    // modelViewMatrix = mult(modelViewMatrix, rotateZ(craneAngle[0]))
+    // modelViewMatrix = mult(modelViewMatrix, scalem(upperCrane.x,upperCrane.y,upperCrane.z));
+    // gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+
+    // cur_vertex = drawCrane(cur_vertex);
+
+
+
+    modelViewMatrix = mult(modelViewMatrix, scalem(0.01,0.01,0.01));
+    cur_vertex = drawHumanTorso(cur_vertex);
+
+    modelViewMatrix = mult(modelViewMatrix, translate(0,humanTorso.h,0));
+    cur_vertex = drawHumanHead(cur_vertex);
+
+
+    cur_vertex = drawHumanUpperArmLeft(cur_vertex);    
+    cur_vertex = drawHumanLowerArmLeft(cur_vertex);    
+    cur_vertex = drawHumanUpperArmRight(cur_vertex);    
+    cur_vertex = drawHumanLowerArmRight(cur_vertex);    
+    cur_vertex = drawHumanPelvis(cur_vertex);    
+    cur_vertex = drawHumanThighLeft(cur_vertex);
+    cur_vertex = drawHumanCalfLeft(cur_vertex);
+    cur_vertex = drawHumanFootLeft(cur_vertex);
+    cur_vertex = drawHumanThighRight(cur_vertex);
+    cur_vertex = drawHumanCalfRight(cur_vertex);
+    cur_vertex = drawHumanFootRight(cur_vertex);
 
     window.requestAnimationFrame(render);
 }
